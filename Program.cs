7deda79a04de3a404b83bec3d7a1ee89e05f2fca
Maxim <lyskomax2@mail.ru>
2024-02-21 01:18:18 +0300

    Add main code

--- Program.cs
+++ Program.cs
@@ -25,3 +25,42 @@ string[] FilterStrings(string[] inputArray, int stringLength)
     return filterArray;
 }
 
+while(true)
+{
+    Console.Write("Введите размер массива: ");
+    int sizeInputArray = int.Parse(Console.ReadLine()!);
+    if (sizeInputArray < 1)
+    {
+        Console.WriteLine("Введены некорректные данные для размера массива (число меньше 1).");
+        continue;
+    }
+    string[] inputArray = new string[sizeInputArray];
+    Console.WriteLine("Введите строки для массива");
+    for (int i = 0; i < sizeInputArray; i++)
+    {
+        Console.Write($"Строка [{i + 1}] массива: ");
+        inputArray[i] = Console.ReadLine()!;
+    }
+    while(true)
+    {
+        Console.Write("Введите максимально допустимую длину строки: ");
+        int stringLength = int.Parse(Console.ReadLine()!);
+        if (stringLength < 1)
+        {
+            Console.WriteLine("Введены некорректные данные для длины строки (число меньше 1)");
+            continue;
+        }
+        string[] filterArray = FilterStrings(inputArray, stringLength);
+        Console.WriteLine($"Изначальный массив строк: \t[{string.Join(", ", inputArray)}]");
+        if (filterArray.Length == 0)
+        {
+            Console.WriteLine($"В заданном массиве нет строк, длина которых меньше либо равна '{stringLength}'");
+        }
+        else
+        {
+            Console.WriteLine($"Отфильтрованный массив строк: \t[{string.Join(", ", filterArray)}]");
+        }
+        break;
+    }
+    break;
+}
